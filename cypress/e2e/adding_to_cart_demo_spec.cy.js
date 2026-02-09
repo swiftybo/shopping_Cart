b1d11bb80ceb8db2dc5a238986eedb293b686ac8
BoSloth <leungjordan28@gmail.com>
2026-02-09 09:58:32 +0000

    id added to stock para for identification in test. test cases added to test functioning of add to cart functionality from home page

---
+++ cypress/e2e/adding_to_cart_demo_spec.cy.js
@@ -0,0 +1,109 @@
+describe("user able to add in-stock items to cart from homepage catalogue", () => {
+  beforeEach(
+    "beforeEach hook to navigate to homepage at start of each test case",
+    () => {
+      cy.visit("http://localhost:5173/");
+    },
+  );
+
+  // This test case works however, it can be improved by:
+  // 1. relies on 'stock_count' id not changing; POM would address this.
+  // 2. second get query gets generic "button" element which would break if more buttons are introduced. Better to identify with id / class.
+  // it("should be able to add to cart if in stock", () => {
+  //   cy.get("[id='stock_count']")
+  //     .each(function (el, index) {
+  //       const currentStock = el.text().split(" ")[0];
+  //       if (currentStock > 1) {
+  //         cy.wrap(currentStock).as("stockNum");
+  //         cy.wrap(index).as("productIndex");
+  //         return false;
+  //       }
+  //     })
+  //     .then(function () {
+  //       cy.get("button").eq(this.productIndex).click();
+  //       cy.get("[id='stock_count']")
+  //         .eq(this.productIndex)
+  //         .then(function (el) {
+  //           const newStock = el.text().split(" ")[0];
+  //           cy.wrap(+newStock).should("equal", this.stockNum - 1);
+  //         });
+  //     });
+  // });
+
+  // This test case is better:
+  // 1. button is found within the fetched productIcon element, making it more reliable than the above solution.
+  // 2. The final assertion uses regex assertion making use of cypress' retryability.
+  it("should be able to add to cart if in stock", () => {
+    cy.get(".productIcon")
+      .each(function (el, index) {
+        const stockNum = el.find("[id='stock_count']").text().split(" ")[0];
+        if (stockNum > 1) {
+          cy.wrap(stockNum).as("stockNum");
+          cy.wrap(index).as("productIndex");
+          return false;
+        }
+      })
+      .then(function () {
+        cy.get(".productIcon")
+          .eq(this.productIndex)
+          .find(".productIcon_btn")
+          .click();
+        cy.get(".productIcon")
+          .eq(this.productIndex)
+          .find("[id='stock_count']")
+
+          // this should is much better as it utilizes cypress' retryability, more readable and
+          .should("contain", this.stockNum - 1);
+
+        // using 'then()' breaks the loop and no longer uses cypress' benefits. cy.wrap() doesn't work as it usually would here
+        // because a STATIC variable is wrapped, it will never change so cypress' retryability is useless. For this to work better,
+        // I should wrap the element itself from then(). This is the "SOURCE".
+        // .then((element) => {
+        //   const newStock = element.text().split(" ")[0];
+        //   cy.wrap(+newStock).should("equal", this.stockNum - 1);
+        // });
+      });
+  });
+
+  it("should not be able to add to cart when stock reaches 0", () => {
+    cy.get(".productIcon")
+      .each(function (el, index) {
+        const stockNum = el.find("[id='stock_count']").text();
+        if (parseInt(stockNum) === 1) {
+          cy.wrap(index).as("productIndex");
+          return false;
+        }
+      })
+      .then(function () {
+        cy.get(".productIcon")
+          .eq(this.productIndex)
+          .find(".productIcon_btn")
+          .click();
+
+        cy.get(".productIcon")
+          .eq(this.productIndex)
+          .find(".productIcon_btn")
+          .should("be.disabled");
+
+        // This is now out of scope of this test and has been decided to be captured in another test suite which
+        // tests the different "stock messages" based on stock count.
+        // cy.get(".productIcon")
+        //   .eq(this.productIndex)
+        //   .find("[id='stock_count']")
+        //   .should("have.text", "Out of stock");
+
+        // cy.get(".productIcon")
+        //   .eq(this.productIndex)
+        //   .find("[id='stock_count']")
+        //   .invoke("text")
+        //   .should("equal", "Out of stock");
+
+        // This does NOT work as text()
+        // cy.get(".productIcon")
+        //   .eq(this.productIndex)
+        //   .find("[id='stock_count']")
+        //   .text()
+        //   .should("equal", "Out of stock");
+      });
+  });
+});
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,66 +1,64 @@
 import { useFood } from "../contexts/FoodContext";
 
 function ProductItem({ name, stock, product, price }) {
-    const { cart, handleAddToCart, handleIncreaseQuantity } = useFood();
+  const { cart, handleAddToCart, handleIncreaseQuantity } = useFood();
 
-    function addItem(product) {
-        console.log(product);
-        const itemAlreadyExists = cart.some(
-            (item) => item.description === product.description,
-        );
-        if (itemAlreadyExists) {
-            handleIncreaseQuantity(product);
-        } else {
-            handleAddToCart(product);
-        }
-    }
-
-    // This function takes the stock from state and calculates a derived state of
-    // the remaining stock using the original stock (saved in state) and subtracting
-    // from the amount in the cart. This results in it being impossible to add constraints
-    // on less than 0 stock being allowed.
-    function calculateRemainingStock() {
-        const identifiedItemIndex = cart.findIndex(
-            (item) => item.description === name,
-        );
-        let amountInCart = cart[identifiedItemIndex]
-            ? cart[identifiedItemIndex]?.quantity
-            : 0;
-        return stock - amountInCart;
-    }
-
-    const remainingStock = calculateRemainingStock();
-
-    return (
-        <section className="productIcon">
-            <div className="productIcon-top">
-                <h3 className="productIcon-header">{name}</h3>
-                <p className="productIcon-price">£{price}.00</p>
-                <p className="color-grey">
-                    {remainingStock === 0
-                        ? "Out of stock"
-                        : `${remainingStock} in stock`}
-                </p>
-                {remainingStock <= 5 && remainingStock > 2 && (
-                    <div className="productIcon-stock">Almost Gone!</div>
-                )}
-                {remainingStock <= 2 && remainingStock > 0 && (
-                    <div className="productIcon-stock">Last Few in Stock!</div>
-                )}
-            </div>
-            <div className="productIcon-bottom">
-                <button
-                    className={`${
-                        remainingStock === 0 && "disabled-button"
-                    } productIcon_btn product-list-button`}
-                    onClick={() => addItem(product)}
-                    disabled={remainingStock === 0}
-                >
-                    {remainingStock === 0 ? "Out of Stock" : "Add to Cart"}
-                </button>
-            </div>
-        </section>
+  function addItem(product) {
+    console.log(product);
+    const itemAlreadyExists = cart.some(
+      (item) => item.description === product.description,
     );
+    if (itemAlreadyExists) {
+      handleIncreaseQuantity(product);
+    } else {
+      handleAddToCart(product);
+    }
+  }
+
+  // This function takes the stock from state and calculates a derived state of
+  // the remaining stock using the original stock (saved in state) and subtracting
+  // from the amount in the cart. This results in it being impossible to add constraints
+  // on less than 0 stock being allowed.
+  function calculateRemainingStock() {
+    const identifiedItemIndex = cart.findIndex(
+      (item) => item.description === name,
+    );
+    let amountInCart = cart[identifiedItemIndex]
+      ? cart[identifiedItemIndex]?.quantity
+      : 0;
+    return stock - amountInCart;
+  }
+
+  const remainingStock = calculateRemainingStock();
+
+  return (
+    <section className="productIcon">
+      <div className="productIcon-top">
+        <h3 className="productIcon-header">{name}</h3>
+        <p className="productIcon-price">£{price}.00</p>
+        <p className="color-grey" id="stock_count">
+          {remainingStock === 0 ? "Out of stock" : `${remainingStock} in stock`}
+        </p>
+        {remainingStock <= 5 && remainingStock > 2 && (
+          <div className="productIcon-stock">Almost Gone!</div>
+        )}
+        {remainingStock <= 2 && remainingStock > 0 && (
+          <div className="productIcon-stock">Last Few in Stock!</div>
+        )}
+      </div>
+      <div className="productIcon-bottom">
+        <button
+          className={`${
+            remainingStock === 0 && "disabled-button"
+          } productIcon_btn product-list-button`}
+          onClick={() => addItem(product)}
+          disabled={remainingStock === 0}
+        >
+          {remainingStock === 0 ? "Out of Stock" : "Add to Cart"}
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default ProductItem;

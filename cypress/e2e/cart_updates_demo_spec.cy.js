90ef02d11140b179ccc1ff6589dd4183fa75f767
BoSloth <leungjordan28@gmail.com>
2026-02-09 13:39:21 +0000

    added final test case to renamed out-of-stock test to check that the addcart button is disabled when the product is out of stock. New test suite created to test the cart page UI

---
+++ cypress/e2e/cart_updates_demo_spec.cy.js
@@ -0,0 +1,3 @@
+describe(
+  "the cart updates when the user adds items to cart. Cart updates with correct item and correct amount",
+);
--- cypress/e2e/out_of_stock_demo_spec.cy.js
+++ cypress/e2e/out_of_stock_demo_spec.cy.js
@@ -1,4 +1,4 @@
-describe("user able to add in-stock items to cart from homepage catalogue", () => {
+describe("user is unable to add items to cart when product becomes out of stock or is already out of stock", () => {
   beforeEach(
     "beforeEach hook to navigate to homepage at start of each test case",
     () => {
@@ -112,5 +112,69 @@ describe("user able to add in-stock items to cart from homepage catalogue", () =
         //   .text()
         //   .should("equal", "Out of stock");
       });
+  });
+
+  it.skip("should not be able to add to cart if product is already out of stock when customer visits page", () => {
+    let itemFound = false;
+
+    cy.get(".productIcon")
+      // Filter to fetch a productIcon which is 'Out of stock'
+      .each((el) => {
+        if (el.find("[id='stock_count']").text() === "Out of stock") {
+          itemFound = true;
+
+          // assertion to check "Add to cart button is disabled"
+          cy.wrap(el).find(".productIcon_btn").should("be.disabled");
+
+          // assertion to check that the stock count did show as -1 and the product remains as 'Out of stock'.
+          cy.wrap(el)
+            .find("[id='stock_count']")
+            .invoke("text")
+            .should("contain", "Out of stock");
+          return false;
+        }
+      })
+      // fails the test if no 'Out of stock' item exists on the page. In later tests, this is handled by cy.contains() however in
+      // this test, if no item is found, no error is thrown without this catch.
+      .then(() => {
+        if (!itemFound) {
+          throw new Error(
+            "Test failed / incomplete: No 'Out of stock' items found in the catalogue",
+          );
+        }
+      });
+  });
+
+  // This is a shorter way of looking through the productIcons to find one which is "Out of stock". It uses the cy.parent() method to stay within the same fetched productIcon.
+  it.skip("should not be able to add to cart if product is already out of stock when customer visits page", () => {
+    // Filter to fetch a productIcon which is 'Out of stock'
+    cy.contains(".productIcon", "Out of stock")
+
+      // assertion to check "Add to cart button is disabled"
+      .find(".productIcon_btn")
+      .should("be.disabled")
+
+      // parents() is used instead of parent() as the button is a grandchild of the productIcon and parent only goes up 1 level of the DOM tree.
+      .parents(".productIcon")
+
+      // assertion to check that the stock count did show as -1 and the product remains as 'Out of stock'.
+      .find("[id='stock_count']")
+      .invoke("text")
+      .should("contain", "Out of stock");
+  });
+
+  // This is a shorter way of looking through the productIcons to find one which is "Out of stock". It uses Cypress aliases to stay within the same fetched productIcon.
+  it("should not be able to add to cart if product is already out of stock when customer visits page", function () {
+    // Filter to fetch a productIcon which is 'Out of stock' and then assigning it an alias
+    cy.contains(".productIcon", "Out of stock").as("fetchedItem");
+
+    // assertion to check "Add to cart button is disabled"
+    cy.get("@fetchedItem").find(".productIcon_btn").should("be.disabled");
+
+    // assertion to check that the stock count did show as -1 and the product remains as 'Out of stock'.
+    cy.get("@fetchedItem")
+      .find("[id='stock_count']")
+      .invoke("text")
+      .should("contain", "Out of stock");
   });
 });
